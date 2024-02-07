6440b449dba4b26d5064f3c83a855ab5bee9f01e
matheuz-siqueira <matheusbatista.dev@gmail.com>
2024-02-07 13:13:55 -0300

    feat: Atualizando departamento

--- SalesWeb.Mvc/Controllers/DepartamentsController.cs
+++ SalesWeb.Mvc/Controllers/DepartamentsController.cs
@@ -1,6 +1,10 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SalesWeb.Mvc.Models;
+using SalesWeb.Mvc.Models.ViewModels;
 using SalesWeb.Mvc.Services.Contracts;
+using SalesWeb.Mvc.Services.Exceptions;
+
 
 namespace SalesWeb.Mvc.Controllers;
 
@@ -35,5 +39,50 @@ public class DepartamentsController : Controller
         return RedirectToAction(nameof(Index)); 
     }
 
-    
+    public async Task<IActionResult> Edit(int? id) 
+    {
+        if(id is null)
+        {
+            return RedirectToAction(nameof(Error), new { message = "Invalid request"}); 
+        }
+        var departament = await _departamentService.GetByIdAsync(id.Value);
+        if(departament is null)
+        {
+            return RedirectToAction(nameof(Error), new { message = "Departament not found" });
+        }
+        return View(departament); 
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Edit(int? id, Departament departament)
+    {
+        if(!ModelState.IsValid)
+        {
+            return View(departament); 
+        }   
+        if(id.Value != departament.Id) 
+        {
+            return RedirectToAction(nameof(Error), new { message = "Invalid request" }); 
+        }
+        try
+        {
+            await _departamentService.UpdateAsync(departament); 
+            return RedirectToAction(nameof(Index));
+        }
+        catch(DbConcurrencyException e) 
+        {
+            return RedirectToAction(nameof(Error), new { message = e.Message }); 
+        }
+    }
+
+    public IActionResult Error(string message)
+    {
+        var viewModel = new ErrorViewModel
+        {
+            Message = message,
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        };
+        return View(viewModel); 
+    }
 }
